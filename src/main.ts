fea2a33c7e5191c715284d5273c6eaa7945aa49d
Bekzat Gazezov <beka.g0226@gmail.com>
2024-12-19 15:12:57 +0500

    updated:main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
